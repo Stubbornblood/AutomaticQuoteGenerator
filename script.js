e976a91dc2461f3181591480e9ff3068d114f7d2
Stubbornblood <ss3591419@gmail.com>
2022-11-06 02:01:01 +0530

    Quote generator resolve script innerHtml

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
-const authorText = document.getElementById("author");
+const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 let apiQuotes = [];
@@ -9,7 +9,9 @@ let apiQuotes = [];
 function newQuote(){
     // pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
-    authorText.textContent = quote.author;
+    authorText.innerHTML = quote.author;
+    quoteText.innerHTML = quote.text;
+    
 }
 // Get Quotes from API
 async function getQuotes(){
